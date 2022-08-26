ff10a01f45a4f1cb2cf777280fe26a1da1183e4c
Junior Sousa <jrsousadev@gmail.com>
2022-08-26 19:21:06 -0300

    (main): delete auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
-import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/auth.service';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://localhost/nest'), UsersModule],
-  controllers: [AuthController],
-  providers: [AuthService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/auth/auth.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get, Post, Res } from '@nestjs/common';
-
-@Controller('auth')
-export class AuthController {
-  @Post()
-  helloWorld() {
-    return 'Hello world';
-  }
-}
