3eaf98201288fb41b5589b7eed5ec9905819138b
Junior Sousa <jrsousadev@gmail.com>
2022-08-26 22:53:32 -0300

    (main): fix erros

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
-import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/auth.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), UsersModule],
-  controllers: [AuthController],
-  providers: [AuthService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    ConfigModule.forRoot(),
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/users/controllers/create.user.controller.ts
+++ src/users/controllers/create.user.controller.ts
@@ -6,12 +6,16 @@ import {
   Post,
   Res,
 } from '@nestjs/common';
+import { AuthService } from '../auth/auth.service';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UsersService } from '../users.service';
 
 @Controller('users')
 export class CreateUserController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private authService: AuthService,
+  ) {}
 
   @Post()
   async execute(@Res() res, @Body() data: CreateUserDto) {
@@ -19,6 +23,8 @@ export class CreateUserController {
 
     if (userExist) throw new NotFoundException('User is exist');
 
+    data.password = await this.authService.cryptedPassword(data.password);
+  
     const user = await this.usersService.create(data);
 
     return res.status(HttpStatus.CREATED).json(user);
